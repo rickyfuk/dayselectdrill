8c55589455a938e191bfa664cffa79a1b4d47c05
digilau <rickyfukfuk@gmail.com>
2020-07-12 16:54:24 -0700

    revamp the script

--- script.js
+++ script.js
@@ -46,46 +46,87 @@ $(document).ready(function () {
 		console.log(selectDateArray);
 		// enable the month selection
 		$('#monthSelection').removeAttr('disabled');
-		$('#monthSelection').change(function () {
-			// the selected month will drop into the selectedDateArray-month position
-			selectDateArray[1] = $('#monthSelection').val();
-			console.log(selectDateArray);
-			// enable the date selection
-			$('#dateSelection').removeAttr('disabled');
-			// empty the date coloum (in case the user click the month mulitple times)
-			$('#dateSelection').empty();
-			// find out the date for the month and put the date into the selection box
-			maxday = moment(selectDateArray).daysInMonth();
-			console.log(maxday);
-			// recreate the "Choose Date" heading as the drop down is empty
-			var option2 = document.createElement('option');
-			$(option2).text('Choose Date');
-			option2.setAttribute('value', '');
-			option2.setAttribute('disabled', true);
-			option2.setAttribute('selected', true);
-			$('#dateSelection').append(option2);
-			// for loop to load the date into the selection list
-			for (n = 0; n < maxday; n++) {
-				createDateList(n + 1);
-			}
-			// when the date is selected, put the date into the selectedDateArray-date position
-			$('#dateSelection').change(function () {
-				selectDateArray[2] = $('#dateSelection').val();
-				console.log(selectDateArray);
-				selectDate = moment(selectDateArray);
-				selectDisplayDate = moment(selectDate).format('dddd, MMMM Do YYYY');
-				selectDateSave = moment(selectDate).format('MMMM Do YYYY');
-				$('#currentDay').empty();
-				$('#currentDay').append(selectDisplayDate);
-				$('#yearSelection').val('');
-				$('#monthSelection').val('');
-				$('#dateSelection').val('');
-				$('#monthSelection').attr('disabled', true);
-				$('#dateSelection').attr('disabled', true);
-				// console.log(selectDate);
-				// console.log(selectDisplayDate);
-				// console.log(selectDateSave);
-			});
-		});
+		// $('#monthSelection').change(function () {
+		// 	// the selected month will drop into the selectedDateArray-month position
+		// 	selectDateArray[1] = $('#monthSelection').val();
+		// 	console.log(selectDateArray);
+		// 	// enable the date selection
+		// 	$('#dateSelection').removeAttr('disabled');
+		// 	// empty the date coloum (in case the user click the month mulitple times)
+		// 	$('#dateSelection').empty();
+		// 	// find out the date for the month and put the date into the selection box
+		// 	maxday = moment(selectDateArray).daysInMonth();
+		// 	console.log(maxday);
+		// 	// recreate the "Choose Date" heading as the drop down is empty
+		// 	var option2 = document.createElement('option');
+		// 	$(option2).text('Choose Date');
+		// 	option2.setAttribute('value', '');
+		// 	option2.setAttribute('disabled', true);
+		// 	option2.setAttribute('selected', true);
+		// 	$('#dateSelection').append(option2);
+		// 	// for loop to load the date into the selection list
+		// 	for (n = 0; n < maxday; n++) {
+		// 		createDateList(n + 1);
+		// 	}
+		// when the date is selected, put the date into the selectedDateArray-date position
+		// $('#dateSelection').change(function () {
+		// 	selectDateArray[2] = $('#dateSelection').val();
+		// 	console.log(selectDateArray);
+		// 	selectDate = moment(selectDateArray);
+		// 	selectDisplayDate = moment(selectDate).format('dddd, MMMM Do YYYY');
+		// 	selectDateSave = moment(selectDate).format('MMMM Do YYYY');
+		// 	$('#currentDay').empty();
+		// 	$('#currentDay').append(selectDisplayDate);
+		// 	$('#yearSelection').val('');
+		// 	$('#monthSelection').val('');
+		// 	$('#dateSelection').val('');
+		// 	$('#monthSelection').attr('disabled', true);
+		// 	$('#dateSelection').attr('disabled', true);
+		// 	// console.log(selectDate);
+		// 	// console.log(selectDisplayDate);
+		// 	// console.log(selectDateSave);
+		// });
+	});
+
+	$('#monthSelection').change(function () {
+		// the selected month will drop into the selectedDateArray-month position
+		selectDateArray[1] = $('#monthSelection').val();
+		console.log(selectDateArray);
+		// enable the date selection
+		$('#dateSelection').removeAttr('disabled');
+		// empty the date coloum (in case the user click the month mulitple times)
+		$('#dateSelection').empty();
+		// find out the date for the month and put the date into the selection box
+		maxday = moment(selectDateArray).daysInMonth();
+		console.log(maxday);
+		// recreate the "Choose Date" heading as the drop down is empty
+		var option2 = document.createElement('option');
+		$(option2).text('Choose Date');
+		option2.setAttribute('value', '');
+		option2.setAttribute('disabled', true);
+		option2.setAttribute('selected', true);
+		$('#dateSelection').append(option2);
+		// for loop to load the date into the selection list
+		for (n = 0; n < maxday; n++) {
+			createDateList(n + 1);
+		}
+	});
+
+	$('#dateSelection').change(function () {
+		selectDateArray[2] = $('#dateSelection').val();
+		console.log(selectDateArray);
+		selectDate = moment(selectDateArray);
+		selectDisplayDate = moment(selectDate).format('dddd, MMMM Do YYYY');
+		selectDateSave = moment(selectDate).format('MMMM Do YYYY');
+		$('#currentDay').empty();
+		$('#currentDay').append(selectDisplayDate);
+		$('#yearSelection').val('');
+		$('#monthSelection').val('');
+		$('#dateSelection').val('');
+		$('#monthSelection').attr('disabled', true);
+		$('#dateSelection').attr('disabled', true);
+		// console.log(selectDate);
+		// console.log(selectDisplayDate);
+		// console.log(selectDateSave);
 	});
 });
